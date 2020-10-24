049fa78dc4e16c06c0a8814abde1ff8a04415e76
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-10-24 09:43:07 +0900

    showメソッドを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.create({name: 'nameですよ', description: '詳細ですよ'})
+Board.create({name: 'nameですおおお', description: '詳細ですよおおおおお'})
