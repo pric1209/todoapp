ed154d6f9b34867c0fb2d549098d23ec81373ef9
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-11-03 12:44:53 +0900

    showページの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,9 +3,36 @@ class BoardsController < ApplicationController
     @board = Board.first
   end
 
-  def create
+  def new
+    @board = Board.new
+    # @board = current_user.boards.build(board_params)
+    # if @board.save
+    #   redirect_to board_path(@board_params), notice:'保存できたよ'
+    # else
+    #   flash.now[:error] = '保存に失敗しました'
+    #   render :new
+    # end
   end
 
+  def create 
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board_params), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+
   def show
+    @board = Board.find(params[:id])
+  end
+
+  private 
+  def board_params
+    puts '===================='
+    puts params
+    params.require(:board).permit(:title, :description)
   end
 end
