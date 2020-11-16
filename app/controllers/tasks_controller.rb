f14ecf29ae32fb3a71c0249faeb5ff0f8aaac84a
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-11-16 22:01:16 +0900

    アイキャッチ画像の設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,16 +4,37 @@ class TasksController < ApplicationController
     end
 
     def create
+        binding.pry
         @task = current_user.tasks.build(task_params)
         if @task.save
-        redirect_to task_path(@task)
+          redirect_to task_path(@task)    
         else
-        render :new
+          render :new
         end
     end
 
     def show
         @task = Task.find(params[:id])
+    end
+
+    def edit
+        @task = current_user.tasks.find(params[:id])
+    end
+    
+    def update
+      @task = current_user.tasks.find(params[:id])
+      if @task.update(task_params)
+        # binding.pry
+        redirect_to task_path(@task)
+      else
+        render :edit
+      end
+    end
+    
+    def destroy
+      task = current_user.tasks.find(params[:id])
+      task.destroy!
+      redirect_to tasks_path
     end
 
     private 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
     has_one_attached :eyecatch
+    
     validates :title, presence: true
     validates :description, presence: true
     validates :deadline, presence: true
 
     belongs_to :user
+    belongs_to :board, optional: true
 end
