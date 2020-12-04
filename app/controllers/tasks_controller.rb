5c8e9337b932d6fbad5d321eae30d75d24b7c4d8
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-12-04 22:04:23 +0900

    task showエラーの解消

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,8 @@ class TasksController < ApplicationController
     end
 
     def show
-      @task = tasks.find(params[:id])
+      binding.pry
+      @task = Task.find(params[:id])
     end
 
     def edit
