15e4739eb10d6b90aa10bfda5df9b5d9ca566a57
江﨑結音 <ezaki@ezaki-yune-no-MacBook-Air.local>
2020-12-13 13:30:19 +0900

    プロフィール機能の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :boards, dependent: :destroy
   has_many :tasks, dependent: :destroy
-  has_one :profile, depenednr: :destroy
+  # has_one :profile, depenednt: :destroy
 end
